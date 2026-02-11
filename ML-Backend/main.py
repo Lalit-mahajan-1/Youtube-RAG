798a6e57b089e08b133ab1253db293d61a261e40
Lalit Mahajan <lalitmahajan444@gmail.com>
2026-02-11 19:28:15 +0530

    cors fixed

--- ML-Backend/main.py
+++ ML-Backend/main.py
@@ -49,9 +49,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
-    "http://localhost:5174",
-    "https://youtube-rag-alpha.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
